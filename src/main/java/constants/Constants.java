d62a47e6f8fa6f9e7a0f5d6962ed68198f5c439f
SwapnaDC <dcleetusswapna@gmail.com>
2023-04-08 12:20:03 +0530

    First Commit

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,14 @@
+package constants;
+
+public class Constants
+{
+	public static String CONFIGfILE="\\src\\main\\java\\Resources\\config.properties";
+	public static String EXCELFILE="\\src\\main\\java\\Resources\\TestData.xlsx";
+	public static String TESTDATAFILE="\\src\\main\\java\\resources\\Testdata.properties";
+	public static String CHROMEDRIVER="src\\main\\java\\Resources\\chromedriver.exe";
+	//public static String PAGEUTILITYFILE="\\src\\main\\java\\Utilities\\PageUtility.java";
+	//public static String EXCELUTILITYFILE="\\src\\main\\java\\Utilities\\ExcelUtility.java";
+	//public static String WAITUTILITYFILE="\\src\\main\\java\\Utilities\\WaitUtility.java";
+	
+	
+}
